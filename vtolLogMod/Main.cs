1093d408519c3fbbb10cc5ba01113f559f4e8d41
Brillcrafter <110330962+Brillcrafter@users.noreply.github.com>
2025-10-07 10:21:40 +0100

    initial commit

---
+++ vtolLogMod/Main.cs
@@ -0,0 +1,24 @@
+﻿global using static vtolLogMod.Logger;
+using System.IO;
+using System.Reflection;
+using ModLoader.Framework;
+using ModLoader.Framework.Attributes;
+
+namespace vtolLogMod;
+
+[ItemId("Brillcrafter.vtolLogMod")] // Harmony ID for your mod, make sure this is unique
+public class Main : VtolMod
+{
+    public string ModFolder;
+
+    private void Awake()
+    {
+        ModFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        Log($"Awake at {ModFolder}");
+    }
+
+    public override void UnLoad()
+    {
+        // Destroy any objects
+    }
+}
